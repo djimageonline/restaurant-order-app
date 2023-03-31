708067a0dca93b0747d29b07a375a98451845d51
Johnny Proano <jproano22@yahoo.com>
2023-03-31 12:05:09 -0400

    added event listner to return proper item comparing dataset.add and menuItem.id, created handleadd menu ffunction

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 import { menuArray } from "/data.js";
 
+document.addEventListener("click", function (e) {
+  if (e.target.dataset.add) {
+    console.log(e.target.dataset);
+    handleAddMenuItem(e.target.dataset.add);
+  }
+});
+
+function handleAddMenuItem(addId) {
+  const menuItemObject = menuArray.filter(function (menuItem) {
+    console.log("menuObj:", menuItem.id, "addbtn:", addId);
+    return menuItem.id === Number(addId);
+  })[0];
+
+  console.log(menuItemObject);
+}
+
 function getOrderMenuHtml() {
   let orderHtml = "";
 
@@ -9,10 +25,13 @@ function getOrderMenuHtml() {
       <div class="menu-item-inner">
         <p class="emoji">${menuItem.emoji}</p>
         <div class="menu-item-container">
-          <h2 class="name">${menuItem.name}</h2>
-          <p class="ingredients">${menuItem.ingredients}</p>
-          <p class="price">$${menuItem.price}</p>
+          <span class="menu-item-detail">
+            <h2 class="name">${menuItem.name}</h2>
+            <p class="ingredients">${menuItem.ingredients}</p>
+            <p class="price">$${menuItem.price}</p>
+          </span>
         </div>
+        <img class="add-btns" src="img/add-btn.png" data-add="${menuItem.id}"></img>
       </div>
     </div>
     
