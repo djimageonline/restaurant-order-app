f491d77090209780848d50293c1b82fb949ff3f3
Johnny Proano <jproano22@yahoo.com>
2023-03-30 21:14:24 -0400

    created getOrdefuncationhtml and render function to iterate of data.js and render item name, ingredients and price in html order-menu

---
+++ index.js
@@ -0,0 +1,27 @@
+import { menuArray } from "/data.js";
+
+function getOrderMenuHtml() {
+  let orderHtml = "";
+
+  menuArray.forEach(function (menuItem) {
+    orderHtml += `
+    <div class="menu-item">
+      <div class="menu-item-inner">
+        <div class="menu-item-container">
+        <h2 class="name">${menuItem.name}</h2>
+        <p class="ingredients">${menuItem.ingredients}</p>
+        <p class="price">$${menuItem.price}</p>
+        </div>
+      </div>
+    </div>
+    
+    `;
+  });
+  return orderHtml;
+}
+
+function render() {
+  document.getElementById("order-menu").innerHTML = getOrderMenuHtml();
+}
+
+render();
